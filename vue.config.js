64b7bfdfeab06e36343f4c01f2512b3be3646ffe
Ciallo <58930257+595152660@users.noreply.github.com>
2020-12-28 17:36:41 +0800

    消息

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    reslove: {
+      alias: {
+        assets: 'src/assets',
+        common: 'src/common',
+        component: 'src/component',
+        network: 'src/network',
+        views: 'src/views',
+
+      }
+    }
+  }
+}
